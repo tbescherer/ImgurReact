7f7a9471f60990047756ebea3efb84e53d66ed07
tom bescherer <tbescherer@gmail.com>
2015-09-02 14:54:51 -0400

    Complete project.

---
+++ src/components/comment-box.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    console.log("hello!")
+    return (
+      <ul className="list-group">
+        {this.renderComments()}
+      </ul>
+    )
+  },
+
+  renderComments: function() {
+    return (
+      this.props.comments.slice(0, 20).map(function(comment) {
+        return <li className="list-group-item comment-box" key={comment.id}>
+          <span className="badge">{comment.ups}</span>
+          <h5>{comment.author}</h5>
+          {comment.comment}
+        </li>
+      })
+    )
+  }
+})
---
+++ src/components/image-preview.jsx
@@ -0,0 +1,65 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      hovering: false
+    }
+  },
+
+  render: function() {
+    return (
+      <Link
+        to={"images/" + this.props.id}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        className="image-preview">
+      {this.props.animated && this.state.hovering ? this.video() : this.image()}
+      {this.props.animated && !this.state.hovering ? this.icon() : null}
+      {this.state.hovering ? this.inset() : null}
+    </Link>
+    )
+  },
+
+  inset: function() {
+    return (
+      <div className="inset">
+        Views: {this.props.views}
+        <br />
+        Upvotes: {this.props.ups}
+      </div>
+    )
+  },
+
+  image: function() {
+    var link = 'http://i.imgur.com/' + this.props.id + "h.jpg";
+
+    return <img src={link} />
+  },
+
+  handleMouseEnter: function () {
+    this.setState({hovering: true})
+  },
+
+  handleMouseLeave: function() {
+    this.setState({hovering: false})
+  },
+
+  video: function() {
+    return (
+      <div>
+        <video preload="auto" autoPlay='autoplay' loop='loop' webkit-playsinline>
+          <source src={this.props.mp4} type='video/mp4' />
+        </video>
+      </div>
+    )
+  },
+
+  icon: function() {
+    return (
+      <span className="glyphicon glyphicon-play"></span>
+    )
+  }
+})
